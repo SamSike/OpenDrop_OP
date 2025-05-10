c0575802f79c5e2622029434d2ff67285b4d4957
Morty931 <141844096+Morty931@users.noreply.github.com>
2025-05-10 15:34:48 +1000

    Feat: Create tooltip_util.py

---
+++ utils/tooltip_util.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+
+def create_tooltip(widget, text):
+    tooltip = tk.Toplevel(widget)
+    tooltip.withdraw()
+    tooltip.overrideredirect(True)
+    label = tk.Label(tooltip, text=text, background="#ffffe0", relief="solid", borderwidth=1, font=("Arial", 10))
+    label.pack()
+
+    def enter(event):
+        tooltip.geometry(f"+{event.x_root + 10}+{event.y_root + 10}")
+        tooltip.deiconify()
+
+    def leave(event):
+        tooltip.withdraw()
+
+    widget.bind("<Enter>", enter)
+    widget.bind("<Leave>", leave)
