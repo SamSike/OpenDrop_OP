7709c8f8683d8845c4793898fce50d7c538786ee
Rin <lvqq2000@gmail.com>
2025-05-24 17:02:03 +1000

    feat(ift analysis table): add red highlight for indicator

--- views/ca_analysis.py
+++ views/ca_analysis.py
@@ -1,6 +1,6 @@
 from modules.core.classes import ExperimentalSetup, ExperimentalDrop
-from modules.preprocessing.ExtractData import ExtractedData
 from utils.image_handler import ImageHandler
+from views.component.imageGallery import ImageGallery
 from utils.config import LEFT_ANGLE, RIGHT_ANGLE, BASELINE_INTERCEPTS, CONTACT_POINTS, TANGENT_LINES, FIT_SHAPE, BASELINE, FIT_SHAPE
 from utils.enums import FittingMethod
 from views.helper.theme import get_system_text_color
